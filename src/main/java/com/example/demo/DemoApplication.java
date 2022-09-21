d0f3bd81cb7c2a0c3752201d56e18a0d8b0969de
Arjun Singh <arjun.singh@navyuginfo.com>
2022-09-21 14:06:16 +0530

    testing pipeline

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hi aru");
-		System.out.println("Hello from arjun side to sarthak");
 		System.out.println("hi arjun");
-		System.out.println("hey bhai");
 		System.out.println("Build pipeline");
-		System.out.println("hello mohit ");
+		System.out.println("Testing pipeline");
 	}
 
 }
